005f349d60ea37ce2fc89ee8bcea1429787bcb7c
Rafael Antonio Gavidia Montaña <ragavidia@misena.edu.co>
2021-09-06 06:56:52 -0500

    Cambios al js

---
+++ vista/js/mian.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+
+    function limpiarCampos(){
+
+
+
+
+    }
+
+    function listarPersonal(){
+
+
+
+
+
+    }
+
+    // Registrar Personal
+
+    $("#").click(function(){
+
+
+
+
+
+
+
+    })
+
+    // Modificar Personal
+
+
+    $("#").on("click", "#", function(){
+
+
+
+
+
+
+    })
+
+
+
+    $("#"))
+
+
+})
